6353d99ae20480c95c14cefaee119ff84e6028d4
Anton Pollak <antoncarlospollak@gmail.com>
2024-12-11 20:52:18 +0100

    continued implementation of task 4.3 (candidate keypoints)

--- process_frame.py
+++ process_frame.py
@@ -2,6 +2,8 @@ import cv2
 import os
 import numpy as np
 
+L_m = 4
+
 # TODO:
 # - might need to pass lk_params to cv2.calcOpticalFlowPyrLK in order to optimize tracking for our case
 def processFrame(img, img_prev, S_prev) -> tuple[dict, np.ndarray]:
@@ -15,6 +17,7 @@ def processFrame(img, img_prev, S_prev) -> tuple[dict, np.ndarray]:
         S: state of current frame (i.e., the keypoints in the current frame and the 3D landmarks associated to them)
         T_WC: current pose
     """
+    # ------------------------------------------------------ 4.1: Associating keypoints
     # track keypoints from previous frame to current frame with KLT (i.e. pixel coordinates)
     keypoints_prev = S_prev["keypoints"]
     object_points = S_prev["landmarks"]
@@ -26,6 +29,7 @@ def processFrame(img, img_prev, S_prev) -> tuple[dict, np.ndarray]:
     keypoints = keypoints[status == 1] # dim: Kx2
     object_points = object_points[status == 1] # dim: Kx3
 
+    # ------------------------------------------------------ 4.2: Estimating current pose
     # extract the pose using P3P
     K = np.loadtxt(os.path.join("", "data_VO/K.txt")) # camera matrix
     _, rvec_CW, tvec_CW, inliers = cv2.solvePnPRansac(objectPoints=object_points, imagePoints=keypoints, cameraMatrix=K, distCoeffs=None, flags=cv2.SOLVEPNP_P3P) # rvec, tvec are the rotation and translation vectors from world frame to camera frame
@@ -33,14 +37,54 @@ def processFrame(img, img_prev, S_prev) -> tuple[dict, np.ndarray]:
     keypoints = keypoints[inliers].squeeze().T # dim: 2xK
     object_points = object_points[inliers].squeeze().T # dim: 3xK
 
-    S = {
-            "keypoints": keypoints, # dim: 2xK
-            "landmarks": S_prev["landmarks"] # dim: 3xK
-        }
     
     rotation_matrix_CW, _ = cv2.Rodrigues(rvec_CW)
     rotation_matrix_WC = rotation_matrix_CW.T
     tvec_WC = -tvec_CW
-    T_WC = np.vstack((np.hstack((rotation_matrix_WC, tvec_WC)), np.array([0, 0, 0, 1])))
+    T_WC = np.hstack((rotation_matrix_WC, tvec_WC))
+
+
+    # ------------------------------------------------------ 4.3: Triangulating new landmarks
+    # ------------------ Check existing candidate keypoints from previous frame(s)
+
+    candidate_keypoints_prev = S_prev["candidate_keypoints"]
+    candidate_keypoints_prev = candidate_keypoints_prev.T.reshape(-1, 1, 2)
+
+    candidate_keypoints, status, _ = cv2.calcOpticalFlowPyrLK(prevImg=img_prev, nextImg=img, prevPts=candidate_keypoints_prev, nextPts=None)
+    candidate_keypoints = candidate_keypoints[status == 1] # dim: Kx2
+    candidate_keypoints_prev = candidate_keypoints_prev[status == 1]
+
+    # ------------------ Promote keypoints
+    S_prev["first_observations"] = S_prev["first_observations"][status == 1] + 1
+
+
+    promoted_keypoints = candidate_keypoints[S_prev["first_observations"] > L_m]
+    n_promoted_keypoints = promoted_keypoint_poses.shape[0] # ????????????????????????
+    keypoints = np.vstack((keypoints, candidate_keypoints[S_prev["first_observations"] > L_m]))
+    candidate_keypoints = candidate_keypoints[S_prev["first_observations"] <= L_m]
+
+    promoted_keypoint_poses_prev = S_prev["pose_at_first_observation"][S_prev["first_observations"] > L_m]
+    promoted_keypoint_poses = np.tile(T_WC,(n_promoted_keypoints,1))
+    # TODO: CHECK DIMENSIONS OF INPUT TO FUNCTION
+    promoted_candidate_landmarks = cv2.triangulatePoints(promoted_keypoint_poses_prev,promoted_keypoint_poses, candidate_keypoints_prev[S_prev["first_observations"] > L_m], promoted_keypoints)
+
+    landmarks = np.vstack((landmarks, promoted_candidate_landmarks))
+
+    
+    S = {}
+
+    S["pose_at_first_observation"] = S_prev["pose_at_first_observation"][S_prev["first_observations"] <= L_m]
+    S["first_observations"] = S_prev["first_observations"][S_prev["first_observations"] <= L_m]
+
+    # ------------------ Extract new keypoints and remove duplicates
+
+
+    S = {
+            "keypoints": keypoints, # dim: 2xK
+            "landmarks": S_prev["landmarks"] # dim: 3xK
+            "candidate_keypoints": .. # dim: 2xM with M = # candidate keypoints
+            "first_observations": ... # dim: 2xM with M = # candidate keypoints
+            "pose_at_first_observation": ... # dim: 12xM with M = # candidate keypoints and 12 since the transformation matrix has 12 entries
+        }
 
     return S, T_WC
