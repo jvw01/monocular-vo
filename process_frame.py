534018333cbb138bda67db18227d5e9e84acb1fa
jakob <jakob.wolfram99@gmail.com>
2024-12-11 19:41:37 +0100

    first draft 4.2 implemented

--- process_frame.py
+++ process_frame.py
@@ -17,29 +17,30 @@ def processFrame(img, img_prev, S_prev) -> tuple[dict, np.ndarray]:
     """
     # track keypoints from previous frame to current frame with KLT (i.e. pixel coordinates)
     keypoints_prev = S_prev["keypoints"]
+    object_points = S_prev["landmarks"]
     keypoints_prev = keypoints_prev.T.reshape(-1, 1, 2) # calcOpticalFlowPyrLK expects shape (N, 1, 2) where N is the number of keypoints
+    object_points = object_points.T.reshape(-1, 1, 3) # shape (N, 1, 3)
     keypoints, status, _ = cv2.calcOpticalFlowPyrLK(prevImg=img_prev, nextImg=img, prevPts=keypoints_prev, nextPts=None)
 
     # filter valid keypoints (note: status is set to 1 if the flow for the corresponding features has been found)
-    keypoints_prev = keypoints_prev[status == 1].T # dim: 2xK
+    keypoints = keypoints[status == 1] # dim: Kx2
+    object_points = object_points[status == 1] # dim: Kx3
 
     # extract the pose using P3P
     K = np.loadtxt(os.path.join("", "data_VO/K.txt")) # camera matrix
-    # cv2.solvePnPRansac
-    # _, rotation_vectors, translation_vectors = cv2.solveP3P(landmark_sample, keypoint_sample.T, K, None, flags=cv2.SOLVEPNP_P3P)
-    # t_C_W_guess = []
-    # R_C_W_guess = []
-    # for rotation_vector in rotation_vectors:
-    #     rotation_matrix = cv2.Rodrigues(rotation_vector)[0]
-    #     for translation_vector in translation_vectors:
-    #         R_C_W_guess.append(rotation_matrix)
-    #         t_C_W_guess.append(translation_vector)
+    _, rvec_CW, tvec_CW, inliers = cv2.solvePnPRansac(objectPoints=object_points, imagePoints=keypoints, cameraMatrix=K, distCoeffs=None, flags=cv2.SOLVEPNP_P3P) # rvec, tvec are the rotation and translation vectors from world frame to camera frame
+    
+    keypoints = keypoints[inliers].squeeze().T # dim: 2xK
+    object_points = object_points[inliers].squeeze().T # dim: 3xK
 
     S = {
-            "keypoints": keypoints,
-            "landmarks": S_prev["landmarks"]
+            "keypoints": keypoints, # dim: 2xK
+            "landmarks": S_prev["landmarks"] # dim: 3xK
         }
     
-    T_WC = None
+    rotation_matrix_CW, _ = cv2.Rodrigues(rvec_CW)
+    rotation_matrix_WC = rotation_matrix_CW.T
+    tvec_WC = -tvec_CW
+    T_WC = np.vstack((np.hstack((rotation_matrix_WC, tvec_WC)), np.array([0, 0, 0, 1])))
 
     return S, T_WC
